b7504b53487dab0a42425c99351fbb28d6e8bc9f
zhiwen.wang <zhiwen.wang@airudder.com>
2026-02-04 10:18:19 +0800

    feat: 主题切到 GitHub Light
    - 新增 github-nvim-theme\n- 默认 colorscheme 改为 github_light（light）

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -6,7 +6,14 @@ return {
     priority = 1000,
   },
   {
+    "projekt0n/github-nvim-theme",
+    name = "github-theme",
+    lazy = false,
+    priority = 1000,
+    config = function() require("github-theme").setup {} end,
+  },
+  {
     "AstroNvim/astroui",
-    opts = function(_, opts) opts.colorscheme = "flexoki-light" end,
+    opts = function(_, opts) opts.colorscheme = "github_light" end,
   },
 }
